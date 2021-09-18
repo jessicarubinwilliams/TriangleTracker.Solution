04493bc1d22c473e0239d73a0cb9dcd6c72dbb90
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-18 00:47:04 -0400

    Add code for console application

--- TriangleTracker/Program.cs
+++ TriangleTracker/Program.cs
@@ -7,7 +7,18 @@ namespace TriangleTracker
   {
     public static void Main()
     {
-
+      Console.WriteLine("Determine if 3 lengths make a triangle.");
+      Console.WriteLine("Length of side 1: ");
+      string stringSide1 = Console.ReadLine();
+      int side1 = int.Parse(stringSide1);
+      Console.WriteLine("Length of side 2: ");
+      string stringSide2 = Console.ReadLine();
+      int side2 = int.Parse(stringSide2);
+      Console.WriteLine("Length of side 3: ");
+      string stringSide3 = Console.ReadLine();
+      int side3 = int.Parse(stringSide3);
+      Triangle userTriangle = new Triangle(side1, side2, side3);
+      Console.WriteLine(userTriangle.CheckType());
     }
   }
 }
