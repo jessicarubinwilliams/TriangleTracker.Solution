3d10d6f37820cb2cfe8f1c5922a05eeb8b50a7f4
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-17 21:13:56 -0400

    Add passing test for TriangleConstructor_ReturnsSide1_Int()

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -10,9 +10,18 @@ namespace TriangleTracker.Tests
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(2);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
-    
+
+    [TestMethod]
+    public void TriangleConstructor_ReturnsSide1_Int()
+    {
+      int side1 = 2;
+      Triangle newTriangle = new Triangle(side1);
+      int result = newTriangle.Side1;
+      Assert.AreEqual(side1, result);
+    }
+
   }
 }
--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -2,6 +2,11 @@ namespace TriangleTracker.Models
 {
   public class Triangle
   {
-    
+    public int Side1 { get; }
+
+    public Triangle(int side1)
+    {
+      Side1 = side1;
+    }
   }
 }
